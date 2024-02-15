21efbd21bb0200794b5d680de280b6bc14f496ae
YVelay <yohan.velay@ynov.com>
2024-02-15 15:56:40 +0100

    added swagger-ui

--- server.js
+++ server.js
@@ -2,4 +2,5 @@ const app = require('./app');
 
 app.listen(3000, () => {
     console.log("Server running on http://localhost:3000");
+    console.log("API documentation on http://localhost:3000/api/v1/documentation");
 });
