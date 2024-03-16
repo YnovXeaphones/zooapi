ae60b4c0ab8d82762af1da5537affd80989a86a1
YVelay <yohan.velay@ynov.com>
2024-03-16 17:10:41 +0100

    Added auth to ZooController

--- routers/zooRouter.js
+++ routers/zooRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const zooController = require('../controllers/zooController');
+const verifyToken = require('../middlewares/verifyToken');
 
 const router = express.Router();
 
-router.post('/createZoo', zooController.createZoo)
-router.put('/updateZoo', zooController.updateZooById);
-router.delete('/deleteZoo', zooController.deleteZooById);
+router.post('/createZoo', zooController.createZoo);
+router.put('/updateZoo', verifyToken, zooController.updateZoo);
+router.delete('/deleteZoo', verifyToken, zooController.deleteZoo);
 
 module.exports = router;
--- services/zooService.js
+++ services/zooService.js
@@ -1,4 +1,4 @@
-const { Zoo } = require('../model/indexModel');
+const { Zoo, User } = require('../model/indexModel');
 
 exports.createZoo = async (name, date) => {
     try {
@@ -10,16 +10,20 @@ exports.createZoo = async (name, date) => {
     }
 };
 
-exports.updateZooById = async (id, name, date) => {
+exports.updateZooById = async (id, name = null) => {
     const zoo = await Zoo.findByPk(id);
     if (!zoo) {
         return null;
     }
-    await zoo.update({ name, date });
+    await zoo.update({ name: name || zoo.name });
     return zoo;
 };
 
 exports.deleteZooById = async (id) => {
+    await User.destroy({ 
+        where: { zooId: id }
+     });
+
     const count = await Zoo.destroy({
         where: { id }
     });
