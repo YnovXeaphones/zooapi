2217a7c0fc1a438e6663aab5d208fe8c83d89f65
YVelay <yohan.velay@ynov.com>
2024-03-16 19:43:28 +0100

    Animal auth

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -1,42 +1,125 @@
 const animalService = require('../services/animalService');
+const cageService = require('../services/cageService');
 
-exports.getAllAnimals = (req, res) => {
-    res.json(animalService.getAllAnimals());
+exports.getAllAnimals = async (req, res) => {
+    if (!req.access.toLowerCase().includes('r')) {
+        return res.status(403).send('Unauthorized');
+    }
+    const zooId = req.zooId;
+    try {
+        const cages = await cageService.getAllCages(zooId);
+        let animals = [];
+
+        for (const cage of cages) {
+            const cageAnimals = await animalService.getAnimalsByCageId(cage.id);
+            animals = animals.concat(cageAnimals);
+        }
+
+        res.json(animals);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
 };
 
-exports.getAnimalById = (req, res) => {
+exports.getAnimalById = async (req, res) => {
+    if (!req.access.toLowerCase().includes('r')) {
+        return res.status(403).send('Unauthorized');
+    }
+    const zooId = req.zooId;
     const id = parseInt(req.params.id);
-    const animal = animalService.getAnimalById(id);
-    if (animal) {
+
+    try {
+        const animal = await animalService.getAnimalById(id);
+        const cage = await cageService.getCageById(animal.cageId, zooId)
+
+        if (!cage || !animal) {
+            return res.status(404).send('Animal Not found');
+        }
+
         res.json(animal);
-    } else {
-        res.status(404).send('Animal not found');
+    } catch (error) {
+        res.status(500).send(error.message);
     }
 };
 
-exports.createAnimal = (req, res) => {
+exports.createAnimal = async (req, res) => {
+    if (!req.access.toLowerCase().includes('c')) {
+        return res.status(403).send('Unauthorized');
+    }
+    const zooId = req.zooId;
     const { name, specie, diet, cageId } = req.body;
-    const animal = animalService.createAnimal(name, specie, diet, cageId);
-    res.status(201).json(animal);
-};
 
-exports.updateAnimalById = (req, res) => {
-    const id = parseInt(req.params.id);
-    const { name, specie, diet, cageId } = req.body;
-    const updatedAnimal = animalService.updateAnimalById(id, name, specie, diet, cageId);
-    if (updatedAnimal) {
-        res.json(updatedAnimal);
-    } else {
-        res.status(404).send('Animal not found');
+    try {
+        const cage = await cageService.getCageById(cageId, zooId);
+        if (!cage) {
+            return res.status(404).send('Cage Not found');
+        }
+
+        const animal = await animalService.createAnimal(name, specie, diet, cageId);
+        
+        res.status(201).json(animal);
+    } catch (error) {
+        res.status(500).send(error.message);
     }
 };
 
-exports.deleteAnimalById = (req, res) => {
+exports.updateAnimalById = async (req, res) => {
+    if (!req.access.toLowerCase().includes('c')) {
+        return res.status(403).send('Unauthorized');
+    }
+    const zooId = req.zooId;
     const id = parseInt(req.params.id);
-    const success = animalService.deleteAnimalById(id);
-    if (success) {
-        res.status(200).send('Animal deleted');
-    } else {
-        res.status(404).send('Animal not found');
+    const { name, specie, diet, cageId } = req.body;
+
+    try {
+        const animal = await animalService.getAnimalById(id);
+        let cage = await cageService.getCageById(animal.cageId, zooId)
+
+        if (!cage || !animal) {
+            return res.status(404).send('Animal Not found');
+        }
+        
+        if (cageId) {
+            cage = await cageService.getCageById(cageId, zooId);
+
+            if (!cage) {
+                return res.status(404).send('Cage Not found');
+            }
+        }
+
+        const updatedAnimal = await animalService.updateAnimalById(id, name, specie, diet, cageId);
+        if (updatedAnimal) {
+            res.json(updatedAnimal);
+        } else {
+            res.status(404).send('Animal not found');
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
+
+exports.deleteAnimalById = async (req, res) => {
+    if (!req.access.toLowerCase().includes('d')) {
+        return res.status(403).send('Unauthorized');
+    }
+    const zooId = req.zooId;
+    const id = parseInt(req.params.id);
+
+    try {
+        const animal = await animalService.getAnimalById(id);
+        const cage = await cageService.getCageById(animal.cageId, zooId);
+
+        if (!cage || !animal) {
+            return res.status(404).send('Animal Not found');
+        }
+
+        const success = await animalService.deleteAnimalById(id);
+        if (success) {
+            res.status(200).send('Animal deleted');
+        } else {
+            res.status(404).send('Animal not found');
+        }
+    } catch (error) {
+        res.status(500).send(error.message);
     }
 };
